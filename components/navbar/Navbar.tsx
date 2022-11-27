37537e1d0df0b16569715a029434470cf7186d7d
Mijail Piekarz <piekarzmijail@gmail.com>
2022-11-27 16:59:46 -0300

    Add a default connect wallet component that will be rendered if the user is not connected to a wallet
    The button to connect the wallet is not connected yet

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,37 +3,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TextField from 'components/Input/TextField';
 import Section from 'components/Section/Section';
 import { FC } from 'react';
-import { useConnect } from 'wagmi';
+import { useAccount, useConnect } from 'wagmi';
 
 const Navbar: FC = () => {
   const { connect, connectors, isLoading, pendingConnector } = useConnect();
+  const { address } = useAccount();
 
-  return (
-    <Section className="mt-6 mb-12">
-      <div className="min-w-[440px]">
-        <TextField
-          startAdornment={
-            <FontAwesomeIcon
-              icon={'fa-solid fa-magnifying-glass' as IconProp}
-            />
-          }
-          placeHolder="Search by asset, address, domain or protocol"
-        />
-      </div>
-      {connectors?.map((connector) => (
-        <button
-          disabled={!connector.ready}
-          key={connector.id}
-          onClick={() => connect({ connector })}
-        >
-          {connector.name}
-          {isLoading &&
-            pendingConnector?.id === connector.id &&
-            ' (connecting)'}
-        </button>
-      ))}
-    </Section>
-  );
+  const renderComponent = () => {
+    if (address) {
+      <Section className="mt-6 mb-12">
+        <div className="min-w-[440px]">
+          <TextField
+            startAdornment={
+              <FontAwesomeIcon
+                icon={'fa-solid fa-magnifying-glass' as IconProp}
+              />
+            }
+            placeHolder="Search by asset, address, domain or protocol"
+          />
+        </div>
+        {connectors?.map((connector) => (
+          <button
+            disabled={!connector.ready}
+            key={connector.id}
+            onClick={() => connect({ connector })}
+          >
+            {connector.name}
+            {isLoading &&
+              pendingConnector?.id === connector.id &&
+              ' (connecting)'}
+          </button>
+        ))}
+      </Section>;
+    }
+    return null;
+  };
+
+  return renderComponent();
 };
 
 export default Navbar;
--- pages/assets.tsx
+++ pages/assets.tsx
@@ -1,4 +1,7 @@
 import Box from 'components/Box/Box';
+import GeneralDefaultConnectWallet from 'components/general/defaultConnectWallet/DefaultConnectWallet';
+import GeneralDefaultConnectWalletDescription from 'components/general/defaultConnectWallet/Description';
+import GeneralDefaultConnectWalletTitle from 'components/general/defaultConnectWallet/Title';
 import PercentageBar from 'components/percentageBar/PercentageBar';
 import Section from 'components/Section/Section';
 import SocialButtons from 'components/SocialButtons/SocialButtons';
@@ -10,8 +13,11 @@ import TabPanels from 'components/tabs/TabPanels';
 import Tabs from 'components/tabs/Tabs';
 import { NextPage } from 'next';
 import Image from 'next/image';
+import { useAccount } from 'wagmi';
+import assets from '../public/images/assets.png';
 
 const Assets: NextPage = () => {
+  const { address } = useAccount();
   const socialIcons = [
     {
       route: '/icons/twitter-white.png',
@@ -50,131 +56,151 @@ const Assets: NextPage = () => {
     },
   ];
 
-  return (
-    <>
-      <Section className="justify-between gap-8">
-        <Stack className="capitalize">
-          <Stack direction="row" className="gap-10">
-            <Stack direction="row">
-              <div className="relative h-[53px] w-[50px]">
+  const renderPage = () => {
+    if (!address) {
+      return (
+        <GeneralDefaultConnectWallet>
+          <Image src={assets} alt="Tokens Vault" objectFit="contain" />
+          <GeneralDefaultConnectWalletTitle>
+            Manage your assets easy
+          </GeneralDefaultConnectWalletTitle>
+          <GeneralDefaultConnectWalletDescription>
+            Track your crypto portfolio across every wallet you own and{' '}
+            <strong>manage your Tokens, NFTs or DeFi activity.</strong>
+          </GeneralDefaultConnectWalletDescription>
+        </GeneralDefaultConnectWallet>
+      );
+    }
+    return (
+      <>
+        <Section className="justify-between gap-8">
+          <Stack className="capitalize">
+            <Stack direction="row" className="gap-10">
+              <Stack direction="row">
+                <div className="relative h-[53px] w-[50px]">
+                  <Image
+                    src="/icons/portfolio.png"
+                    layout="fill"
+                    alt="portfolio"
+                    objectFit="contain"
+                  />
+                </div>
+                <h3 className="text-gradient h2">Portfolio</h3>
+              </Stack>
+              <span>All networks</span>
+            </Stack>
+            <Stack className="!gap-2.5">
+              <span className="text-blue-gray">Your profile</span>
+              <span className="text-2xl">Juvencrypto</span>
+              <Stack direction="row" className="cursor-pointer">
+                <span className="text-blue-gray">0x797..31A9b</span>
                 <Image
-                  src="/icons/portfolio.png"
-                  layout="fill"
-                  alt="portfolio"
+                  src="/icons/copy.png"
+                  width={13}
+                  height={15}
+                  alt="copy"
                   objectFit="contain"
                 />
-              </div>
-              <h3 className="text-gradient h2">Portfolio</h3>
+              </Stack>
+              <SocialButtons className="mt-3" socialIcons={socialIcons} />
             </Stack>
-            <span>All networks</span>
           </Stack>
-          <Stack className="!gap-2.5">
-            <span className="text-blue-gray">Your profile</span>
-            <span className="text-2xl">Juvencrypto</span>
-            <Stack direction="row" className="cursor-pointer">
-              <span className="text-blue-gray">0x797..31A9b</span>
-              <Image
-                src="/icons/copy.png"
-                width={13}
-                height={15}
-                alt="copy"
-                objectFit="contain"
-              />
+          <Box className="min-w-[334px] basis-2/6 rounded-lg bg-white p-8 capitalize drop-shadow-lg">
+            <Stack className="!gap-1">
+              <h4 className="text-blue-gray">net worth</h4>
+              <span className="text-3xl">$80.00</span>
             </Stack>
-            <SocialButtons className="mt-3" socialIcons={socialIcons} />
-          </Stack>
-        </Stack>
-        <Box className="min-w-[334px] basis-2/6 rounded-lg bg-white p-8 capitalize drop-shadow-lg">
-          <Stack className="!gap-1">
-            <h4 className="text-blue-gray">net worth</h4>
-            <span className="text-3xl">$80.00</span>
-          </Stack>
-          <span className="my-4 block text-blue-gray">Portfolio Breakdown</span>
-          <div className="space-y-4">
-            <PercentageBar name={'Total Assets'} percentage={100} />
-            <PercentageBar name={'On Peace'} percentage={50} />
-          </div>
-        </Box>
-      </Section>
+            <span className="my-4 block text-blue-gray">
+              Portfolio Breakdown
+            </span>
+            <div className="space-y-4">
+              <PercentageBar name={'Total Assets'} percentage={100} />
+              <PercentageBar name={'On Peace'} percentage={50} />
+            </div>
+          </Box>
+        </Section>
 
-      <TabGroup className="mt-14">
-        <Tabs>
-          <Tab>Tokens</Tab>
-          <Tab>Collectibles</Tab>
-          <Tab>Walle Activity</Tab>
-        </Tabs>
-        <TabPanels>
-          <TabPanel>
-            <div className="mt-12 grid grid-cols-2 gap-8">
-              <Box>
-                <TabGroup>
-                  <Stack direction="row" className="justify-between">
-                    <h4 className="block capitalize text-blue-gray">
-                      Performance
-                    </h4>
-                    <Tabs className="!gap-3 text-sm">
-                      <Tab>1D</Tab>
-                      <Tab>1W</Tab>
-                      <Tab>1M</Tab>
-                      <Tab>1Y</Tab>
-                    </Tabs>
-                  </Stack>
-                  <TabPanels>
-                    <TabPanel>
-                      <span className="mb-5 block text-3xl">$80.00</span>
-                      <div className="relative h-48 w-full">
-                        <Image
-                          src="/images/assets-chart.png"
-                          layout="fill"
-                          alt="chart"
-                          objectFit="contain"
-                        />
-                      </div>
-                    </TabPanel>
-                    <TabPanel>
-                      <></>
-                    </TabPanel>
-                    <TabPanel>
-                      <></>
-                    </TabPanel>
-                    <TabPanel>
-                      <></>
-                    </TabPanel>
-                  </TabPanels>
-                </TabGroup>
-              </Box>
-              <Box>
-                <h5 className="capitalize text-blue-gray">
-                  Network Allocation
-                </h5>
-                <div className="mt-6 grid grid-cols-2 gap-x-10 gap-y-12">
-                  {tokens.map((token) => {
-                    return (
-                      <Stack key={token.name} direction="row">
-                        <div className="relative h-8 w-8 shrink-0">
+        <TabGroup className="mt-14">
+          <Tabs>
+            <Tab>Tokens</Tab>
+            <Tab>Collectibles</Tab>
+            <Tab>Walle Activity</Tab>
+          </Tabs>
+          <TabPanels>
+            <TabPanel>
+              <div className="mt-12 grid grid-cols-2 gap-8">
+                <Box>
+                  <TabGroup>
+                    <Stack direction="row" className="justify-between">
+                      <h4 className="block capitalize text-blue-gray">
+                        Performance
+                      </h4>
+                      <Tabs className="!gap-3 text-sm">
+                        <Tab>1D</Tab>
+                        <Tab>1W</Tab>
+                        <Tab>1M</Tab>
+                        <Tab>1Y</Tab>
+                      </Tabs>
+                    </Stack>
+                    <TabPanels>
+                      <TabPanel>
+                        <span className="mb-5 block text-3xl">$80.00</span>
+                        <div className="relative h-48 w-full">
                           <Image
-                            src={token.icon}
+                            src="/images/assets-chart.png"
                             layout="fill"
-                            alt={token.name}
+                            alt="chart"
+                            objectFit="contain"
                           />
                         </div>
-                        <div>
-                          <span className="block whitespace-nowrap text-xs text-blue-gray">
-                            Assets on {token.name}
-                          </span>
-                          <span className="block">${token.amount}</span>
-                        </div>
-                      </Stack>
-                    );
-                  })}
-                </div>
-              </Box>
-            </div>
-          </TabPanel>
-        </TabPanels>
-      </TabGroup>
-    </>
-  );
+                      </TabPanel>
+                      <TabPanel>
+                        <></>
+                      </TabPanel>
+                      <TabPanel>
+                        <></>
+                      </TabPanel>
+                      <TabPanel>
+                        <></>
+                      </TabPanel>
+                    </TabPanels>
+                  </TabGroup>
+                </Box>
+                <Box>
+                  <h5 className="capitalize text-blue-gray">
+                    Network Allocation
+                  </h5>
+                  <div className="mt-6 grid grid-cols-2 gap-x-10 gap-y-12">
+                    {tokens.map((token) => {
+                      return (
+                        <Stack key={token.name} direction="row">
+                          <div className="relative h-8 w-8 shrink-0">
+                            <Image
+                              src={token.icon}
+                              layout="fill"
+                              alt={token.name}
+                            />
+                          </div>
+                          <div>
+                            <span className="block whitespace-nowrap text-xs text-blue-gray">
+                              Assets on {token.name}
+                            </span>
+                            <span className="block">${token.amount}</span>
+                          </div>
+                        </Stack>
+                      );
+                    })}
+                  </div>
+                </Box>
+              </div>
+            </TabPanel>
+          </TabPanels>
+        </TabGroup>
+      </>
+    );
+  };
+
+  return renderPage();
 };
 
 export default Assets;
